c7810e1a99bfb08b3ebaa2c68fc67e5f09cfddb1
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-16 00:53:29 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5088657122:AAHdusGDuWfBpSDWkcX-qU1_fgzij4w8Lzk")
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
-    USER_NAME = os.environ.get("USER_NAME", "")
+    USER_NAME = os.environ.get("USER_NAME", "@haaaaarki")
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
     BANNED_USER = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
     OUO_IO_API_KEY = ""
     MAX_MESSAGE_LENGTH = 4096
-    BOT_PWD = os.environ.get("BOT_PASSWORD", "")
+    BOT_PWD = os.environ.get("BOT_PASSWORD", "3333")
     LOGGED_USER = []
-    DB_URI = os.environ.get("DATABASE_URL", "")
+    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://abirhasan2005:abirhasan@cluster0.i6qzp.mongodb.net/cluster0?retryWrites=true&w=majority")
