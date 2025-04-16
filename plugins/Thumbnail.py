4e35c6e65a7560839aeb3b35d05761200b92a69c
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-16 14:05:54 +0330

    Update Thumbnail.py

--- plugins/Thumbnail.py
+++ plugins/Thumbnail.py
@@ -1,49 +1,35 @@
 import logging
+import os
+
+from config import Config
+from translation import Translation
+import database.database as db
+
+from pyrogram import Client, filters
+
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
-import numpy
-import os
-import time
-
-from config import Config
-from PIL import Image
-from translation import Translation
-
-import database.database as sql
-import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-from pyrogram import Client, Filters 
 
-@Client.on_message(Filters.photo)
+
+@Client.on_message(filters.photo)
 async def save_photo(bot, update):
-    if update.from_user.id in Config.BANNED_USER:
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
+    user_id = update.from_user.id
+
+    if user_id in Config.BANNED_USER:
+        await bot.delete_messages(chat_id=update.chat.id, message_ids=update.message_id, revoke=True)
         return
+
     if update.media_group_id is not None:
-        # album is sent
-        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "/" + str(update.media_group_id) + "/"
-        # create download directory, if not exist
-        if not os.path.isdir(download_location):
-            os.makedirs(download_location)
-        await sql.df_thumb(update.from_user.id, update.message_id)
-        await bot.download_media(
-            message=update,
-            file_name=download_location
-        )
+        download_location = os.path.join(Config.DOWNLOAD_LOCATION, str(user_id), str(update.media_group_id))
+        os.makedirs(download_location, exist_ok=True)
+        await db.df_thumb(user_id, update.message_id)
+        await bot.download_media(message=update, file_name=download_location)
     else:
-        # received single photo
-        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-        await bot.download_media(
-            message=update,
-            file_name=download_location
-        )
-        await sql.df_thumb(update.from_user.id, update.message_id)
+        download_location = os.path.join(Config.DOWNLOAD_LOCATION, f"{user_id}.jpg")
+        await bot.download_media(message=update, file_name=download_location)
+        await db.df_thumb(user_id, update.message_id)
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.SAVED_CUSTOM_THUMB_NAIL,
@@ -51,22 +37,23 @@ async def save_photo(bot, update):
         )
 
 
-@Client.on_message(Filters.command(["deletethumbnail"]))
+@Client.on_message(filters.command(["deletethumbnail"]))
 async def delete_thumbnail(bot, update):
-    if update.from_user.id in Config.BANNED_USER:
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
+    user_id = update.from_user.id
+
+    if user_id in Config.BANNED_USER:
+        await bot.delete_messages(chat_id=update.chat.id, message_ids=update.message_id, revoke=True)
         return
-    download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id)
+
+    download_path = os.path.join(Config.DOWNLOAD_LOCATION, f"{user_id}.jpg")
+
     try:
-        await sql.del_thumb(update.from_user.id)
-        os.remove(download_location + ".jpg")
-        # os.remove(download_location + ".json")
-    except:
-        pass
+        await db.del_thumb(user_id)
+        if os.path.exists(download_path):
+            os.remove(download_path)
+    except Exception as e:
+        logger.warning(f"Error deleting thumbnail: {e}")
+
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.DEL_ETED_CUSTOM_THUMB_NAIL,
