e5e28ad1bad1b8d81f6e6a9d5b9a58cacf081a59
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-16 14:22:17 +0330

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,3 +1,4 @@
+
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -5,80 +6,68 @@ logger = logging.getLogger(__name__)
 
 import pyrogram
 from config import Config 
-from pyrogram import Client, Filters, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client
+from pyrogram.handlers import MessageHandler
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from translation import Translation
 from Tools.Download import download
-
-
 
 my_father = "https://t.me/{}".format(Config.USER_NAME[1:])
 support = "https://telegram.dog/Ns_Bot_supporters"
 
-@Client.on_message(Filters.command(["start"]))
+@Client.on_message(MessageHandler(filters=pyrogram.Filters.command(["start"])))
 async def start(c, m):
-
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.START.format(m.from_user.first_name, Config.USER_NAME),
                          reply_to_message_id=m.message_id,
                          reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("My Father 👨‍💻", url=my_father), InlineKeyboardButton("📌Support channel", url=support)]]))
     logger.info(f"{m.from_user.first_name} used start command")
 
-
-
-@Client.on_message(Filters.command(["help"]))
+@Client.on_message(MessageHandler(filters=pyrogram.Filters.command(["help"])))
 async def help(c, m):
-
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.HELP,
                          reply_to_message_id=m.message_id,
                          parse_mode="markdown")
 
-
-@Client.on_message(Filters.command(["about"]))
+@Client.on_message(MessageHandler(filters=pyrogram.Filters.command(["about"])))
 async def about(c, m):
-
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.ABOUT,
                          disable_web_page_preview=True,
                          reply_to_message_id=m.message_id,
                          parse_mode="markdown")
 
-@Client.on_message(Filters.command(["converttovideo"]))
+@Client.on_message(MessageHandler(filters=pyrogram.Filters.command(["converttovideo"])))
 async def video(c, m):
+    if Config.BOT_PWD:
+        if (m.from_user.id not in Config.LOGGED_USER) & (m.from_user.id not in Config.AUTH_USERS):
+            await m.reply_text(text=Translation.NOT_LOGGED_TEXT, quote=True)
+            return
+    if m.from_user.id in Config.BANNED_USER:
+        await c.send_message(chat_id=m.chat.id, text=Translation.BANNED_TEXT)
+        return
+    if m.from_user.id not in Config.BANNED_USER:
+        if m.reply_to_message is not None:
+            await download(c, m)
+        else:
+            await c.send_message(chat_id=m.chat.id, text=Translation.REPLY_TEXT)
 
-  if Config.BOT_PWD:
-      if (m.from_user.id not in Config.LOGGED_USER) & (m.from_user.id not in Config.AUTH_USERS):
-          await m.reply_text(text=Translation.NOT_LOGGED_TEXT, quote=True)
-          return
-      else:
-          pass
-  if m.from_user.id in Config.BANNED_USER:
-      await c.send_message(chat_id=m.chat.id, text=Translation.BANNED_TEXT)
-      return
-  if m.from_user.id not in Config.BANNED_USER:
-      if m.reply_to_message is not None:
-          await download(c, m)
-      else:
-          await c.send_message(chat_id=m.chat.id, text=Translation.REPLY_TEXT)
-
-@Client.on_message(Filters.command(["converttofile"]))
+@Client.on_message(MessageHandler(filters=pyrogram.Filters.command(["converttofile"])))
 async def file(c, m):
+    if Config.BOT_PWD:
+        if (m.from_user.id not in Config.LOGGED_USER) & (m.from_user.id not in Config.AUTH_USERS):
+            await m.reply_text(text=Translation.NOT_LOGGED_TEXT, quote=True)
+            return
+    if m.from_user.id in Config.BANNED_USER:
+        await c.send_message(chat_id=m.chat.id, text=Translation.BANNED_TEXT)
+    if m.from_user.id not in Config.BANNED_USER:
+        if m.reply_to_message is not None:
+            await download(c, m)
+        else:
+            await c.send_message(chat_id=m.chat.id, text=Translation.REPLY_TEXT)
 
-  if Config.BOT_PWD:
-      if (m.from_user.id not in Config.LOGGED_USER) & (m.from_user.id not in Config.AUTH_USERS):
-          await m.reply_text(text=Translation.NOT_LOGGED_TEXT, quote=True)
-          return
-      else:
-          pass
-  if m.from_user.id in Config.BANNED_USER:
-      await c.send_message(chat_id=m.chat.id, text=Translation.BANNED_TEXT)
-  if m.from_user.id not in Config.BANNED_USER:
-    if m.reply_to_message is not None:
-      await download(c, m)
-    else:
-       await c.send_message(chat_id=m.chat.id, text=Translation.REPLY_TEXT)
-
-@Client.on_message(Filters.command(["login"]))
+@Client.on_message(MessageHandler(filters=pyrogram.Filters.command(["login"])))
 async def login(c, m):
     if Config.BOT_PWD:
         if (len(m.command) >= 2) & (m.from_user.id not in Config.LOGGED_USER) & (m.from_user.id not in Config.AUTH_USERS):
@@ -104,7 +93,7 @@ async def login(c, m):
                                  reply_to_message_id=m.message_id,
                                  parse_mode="markdown")
 
-        if (m.from_user.id in Config.LOGGED_USER)|(m.from_user.id in Config.AUTH_USERS):
+        if (m.from_user.id in Config.LOGGED_USER) | (m.from_user.id in Config.AUTH_USERS):
             await c.send_message(chat_id=m.chat.id,
                                  text=Translation.EXISTING_USER,
                                  disable_web_page_preview=True,
